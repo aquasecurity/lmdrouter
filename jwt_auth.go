f86079561eb8be649e6397ce958dbd598d5bf2bc
seantcanavanpublic@gmail.com <seantcanavanpublic@gmail.com>
2022-07-29 15:13:52 -0500

    remove all mention of admin, add support for generic custom claims, add support for standard claim fields, add support for validating standard claim fields, add way more tests, add helper functions to add and remove custom fields from a custom map, remove all forbidden errors and switch to unauthorized, remove dependency on lfutils

--- jwt_auth.go
+++ jwt_auth.go
@@ -2,50 +2,29 @@ package lmdrouter
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-	"time"
 )
 
-type AdminClaim struct {
-	ExpirationDate time.Time
-	FullName       string
-	ID             string
-	Level          string
-}
+var secret = os.Getenv("HMAC_SECRET")
 
-func GenerateJwt(adminFullName, adminID, adminLevel string, expirationDate *time.Time) (string, int, error) {
-	if adminFullName == "" {
-		return "", 400, fmt.Errorf("unable to generate jwt with empty adminFullName %s", adminFullName)
-	}
+const (
+	AudienceKey  = "aud"
+	ExpiresAtKey = "exp"
+	IDKey        = "jti"
+	IssuedAtKey  = "iat"
+	IssuerKey    = "iss"
+	NotBeforeKey = "nbf"
+	SubjectKey   = "sub"
+)
 
-	if adminID == "" {
-		return "", 400, fmt.Errorf("unable to generate jwt with empty adminID %s", adminID)
-	}
-
-	if adminLevel == "" {
-		return "", 400, fmt.Errorf("unable to generate jwt with empty adminLevel %s", adminLevel)
-	}
-
-	if expirationDate.IsZero() {
-		return "", 400, fmt.Errorf("unable to generate jwt with empty expirationDate %+v", expirationDate)
-	}
-
-	if expirationDate.Before(time.Now()) {
-		return "", 400, fmt.Errorf("unable to generate jwt with expiration before now %+v", expirationDate)
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
-		"adminFullName":  adminFullName,
-		"adminID":        adminID,
-		"adminLevel":     adminLevel,
-		"expirationDate": expirationDate,
-	})
+func GenerateJWT(claims jwt.MapClaims) (string, int, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
 
 	// Sign and get the complete encoded token as a string using the secret
 	encodedToken, err := token.SignedString(getBinarySecret())
@@ -56,80 +35,85 @@ func GenerateJwt(adminFullName, adminID, adminLevel string, expirationDate *time
 	return encodedToken, http.StatusOK, nil
 }
 
-func JwtAuth(headers map[string]string) (*AdminClaim, int, error) {
-	authorizationHeader := headers["Authorization"]
-	if authorizationHeader == "" {
-		return nil, http.StatusForbidden, fmt.Errorf("missing Authorization header value")
-	}
-
-	if !strings.HasPrefix(authorizationHeader, "Bearer ") {
-		return nil, http.StatusForbidden, fmt.Errorf("missing 'Bearer ' prefix for Authorization header")
-	}
-
-	userJwt := strings.TrimPrefix(authorizationHeader, "Bearer ")
-
-	adminClaim, httpStatus, err := Verify(userJwt)
-	if err != nil {
-		return nil, httpStatus, err
-	}
-
-	return adminClaim, http.StatusOK, nil
+// InitializeMapClaims embeds all the necessary values for a StandardClaims inside of custom MapClaims.
+// The returned MapClaims can be used to insert any custom values that you need for your own purpose.
+func InitializeMapClaims(audience string, expiresAt int64, id string, issuedAt int64, issuer string, notBefore int64, subject string) (jwt.MapClaims, error) {
+	return verifyMapClaims(jwt.MapClaims{
+		AudienceKey:  audience,
+		ExpiresAtKey: expiresAt,
+		IDKey:        id,
+		IssuedAtKey:  issuedAt,
+		IssuerKey:    issuer,
+		NotBeforeKey: notBefore,
+		SubjectKey:   subject,
+	})
 }
 
-// Verify accepts the user JWT from the Authorization header
-// and returns the adminID, adminLevel, true/false for success
-// and an error code
-func Verify(userJwt string) (*AdminClaim, int, error) {
-	token, err := jwt.Parse(userJwt, KeyFunc)
-
+func ExtractStandardClaims(mapClaims jwt.MapClaims) (*jwt.StandardClaims, error) {
+	jsonBytes, err := json.Marshal(mapClaims)
 	if err != nil {
-		log.Printf("Error with JWT %+v", err)
-		return nil, http.StatusInternalServerError, err
+		return nil, err
+	}
+
+	var standardClaims jwt.StandardClaims
+	err = json.Unmarshal(jsonBytes, &standardClaims)
+	if err != nil {
+		return nil, err
+	}
+
+	return &standardClaims, nil
+}
+
+func verifyMapClaims(claims jwt.MapClaims) (jwt.MapClaims, error) {
+	if claims["aud"].(string) == "" {
+		return nil, fmt.Errorf("aud (audience) is required")
+	}
+
+	if claims["exp"].(int64) == 0 {
+		return nil, fmt.Errorf("exp (expiresAt) is required")
+	}
+
+	if claims["jti"].(string) == "" {
+		return nil, fmt.Errorf("jti (id) is required")
+	}
+
+	if claims["iat"].(int64) == 0 {
+		return nil, fmt.Errorf("iat (issuedAt) is required")
+	}
+
+	if claims["iss"].(string) == "" {
+		return nil, fmt.Errorf("iss (issuer) is required")
+	}
+
+	if claims["nbf"].(int64) == 0 {
+		return nil, fmt.Errorf("nbf (notBefore) is required")
+	}
+
+	if claims["sub"].(string) == "" {
+		return nil, fmt.Errorf("sub (subject) is required")
+	}
+
+	return claims, nil
+}
+
+// VerifyJWT accepts the user JWT from the Authorization header
+// and returns the MapClaims OR a http status code and error set
+func VerifyJWT(userJWT string) (jwt.MapClaims, int, error) {
+	token, err := jwt.Parse(userJWT, keyFunc)
+	if err != nil {
+		return nil, http.StatusUnauthorized, err
 	}
 
 	if !token.Valid {
-		return nil, http.StatusForbidden, errors.New("token is not valid")
+		return nil, http.StatusUnauthorized, errors.New("token is not valid")
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		adminFullName := claims["adminFullName"].(string)
-		adminID := claims["adminID"].(string)
-		adminLevel := claims["adminLevel"].(string)
-		expirationDate := claims["expirationDate"].(string)
-
-		expirationDateAsTime, err := time.Parse(time.RFC3339, expirationDate)
-		if err != nil {
-			return nil, 500, err
-		}
-
-		if adminID == "" {
-			return nil, http.StatusForbidden, errors.New("adminID cannot be empty")
-		}
-
-		if adminLevel == "" {
-			return nil, http.StatusForbidden, errors.New("adminLevel cannot be empty")
-		}
-
-		if expirationDateAsTime.IsZero() {
-			return nil, http.StatusUnauthorized, errors.New("invalid expiration date")
-		}
-
-		if time.Now().After(expirationDateAsTime) {
-			return nil, http.StatusUnauthorized, errors.New("jwt is expired")
-		}
-
-		return &AdminClaim{
-			ExpirationDate: expirationDateAsTime,
-			FullName:       adminFullName,
-			ID:             adminID,
-			Level:          adminLevel,
-		}, http.StatusOK, nil
+		return claims, http.StatusOK, nil
 	}
 
 	return nil, http.StatusInternalServerError, errors.New("unable to parse MapClaims")
 }
-
-var secret = os.Getenv("HMAC_SECRET")
 
 func getBinarySecret() []byte {
 	data, err := hex.DecodeString(secret)
@@ -140,7 +124,7 @@ func getBinarySecret() []byte {
 	return data
 }
 
-func KeyFunc(token *jwt.Token) (interface{}, error) {
+func keyFunc(token *jwt.Token) (interface{}, error) {
 	// Don't forget to validate the alg is what you expect:
 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
--- jwt_auth_test.go
+++ jwt_auth_test.go
@@ -2,83 +2,193 @@ package lmdrouter
 
 import (
 	"github.com/jgroeneveld/trial/assert"
-	"github.com/learnfully/learnfully/lfutils"
+	"math/rand"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 )
 
-func TestJwtAuth(t *testing.T) {
-	t.Run("no fullName", func(t *testing.T) {
-		adminFullName := ""
-		adminID := lfutils.GenerateRandomString(10)
-		adminLevel := "Developer"
-		expirationDate := time.Now().Add(time.Minute * 1)
-		jwt, httpStatus, err := GenerateJwt(adminFullName, adminID, adminLevel, &expirationDate)
-		assert.Equal(t, httpStatus, http.StatusBadRequest)
-		assert.NotNil(t, err)
-		assert.Equal(t, jwt, "")
-	})
-
-	t.Run("no adminID", func(t *testing.T) {
-		adminFullName := lfutils.GenerateRandomString(10)
-		adminID := ""
-		adminLevel := "Developer"
-		expirationDate := time.Now().Add(time.Minute * 1)
-		jwt, httpStatus, err := GenerateJwt(adminFullName, adminID, adminLevel, &expirationDate)
-		assert.Equal(t, httpStatus, http.StatusBadRequest)
-		assert.NotNil(t, err)
-		assert.Equal(t, jwt, "")
-	})
-
-	t.Run("no adminLevel", func(t *testing.T) {
-		adminFullName := lfutils.GenerateRandomString(10)
-		adminID := lfutils.GenerateRandomString(10)
-		adminLevel := ""
-		expirationDate := time.Now().Add(time.Minute * 1)
-		jwt, httpStatus, err := GenerateJwt(adminFullName, adminID, adminLevel, &expirationDate)
-		assert.Equal(t, httpStatus, http.StatusBadRequest)
-		assert.NotNil(t, err)
-		assert.Equal(t, jwt, "")
-	})
-
-	t.Run("no expirationDate", func(t *testing.T) {
-		adminFullName := lfutils.GenerateRandomString(10)
-		adminID := lfutils.GenerateRandomString(10)
-		adminLevel := "Developer"
-		var expirationDate time.Time
-		jwt, httpStatus, err := GenerateJwt(adminFullName, adminID, adminLevel, &expirationDate)
-		assert.Equal(t, httpStatus, http.StatusBadRequest)
-		assert.NotNil(t, err)
-		assert.Equal(t, jwt, "")
-	})
-
-	t.Run("expirationDate before now (already expired)", func(t *testing.T) {
-		adminFullName := lfutils.GenerateRandomString(10)
-		adminID := lfutils.GenerateRandomString(10)
-		adminLevel := "Developer"
-		expirationDate := time.Now().Add(time.Minute * -10)
-		jwt, httpStatus, err := GenerateJwt(adminFullName, adminID, adminLevel, &expirationDate)
-		assert.Equal(t, httpStatus, http.StatusBadRequest)
-		assert.NotNil(t, err)
-		assert.Equal(t, jwt, "")
-	})
-
-	t.Run("successfully encode JWT, make claim, and Verify JWT and compare values against claim", func(t *testing.T) {
-		adminFullName := lfutils.GenerateRandomString(10)
-		adminID := lfutils.GenerateRandomString(10)
-		adminLevel := "Developer"
-		expirationDate := time.Now().Add(time.Minute * 1)
-		jwt, httpStatus, err := GenerateJwt(adminFullName, adminID, adminLevel, &expirationDate)
-		assert.Equal(t, httpStatus, http.StatusOK)
+func TestInitializeMapClaimsAndExtractStandardClaims(t *testing.T) {
+	t.Run("recover full standard claims", func(t *testing.T) {
+		claims, err := InitializeMapClaims(
+			GenerateRandomString(10),
+			20000,
+			GenerateRandomString(10),
+			20000,
+			GenerateRandomString(10),
+			20000,
+			GenerateRandomString(10),
+		)
 		assert.Nil(t, err)
-		assert.NotEqual(t, jwt, "")
 
-		adminClaim, httpStatus, err := Verify(jwt)
-		assert.Equal(t, httpStatus, http.StatusOK)
+		standardClaims, err := ExtractStandardClaims(claims)
 		assert.Nil(t, err)
-		assert.Equal(t, adminClaim.ID, adminID)
-		assert.Equal(t, adminClaim.Level, adminLevel)
-		assert.True(t, lfutils.AreSameSecond(&adminClaim.ExpirationDate, &expirationDate))
+
+		assert.Equal(t, standardClaims.Audience, claims[AudienceKey])
+		assert.Equal(t, standardClaims.ExpiresAt, claims[ExpiresAtKey])
+		assert.Equal(t, standardClaims.Id, claims[IDKey])
+		assert.Equal(t, standardClaims.IssuedAt, claims[IssuedAtKey])
+		assert.Equal(t, standardClaims.Issuer, claims[IssuerKey])
+		assert.Equal(t, standardClaims.NotBefore, claims[NotBeforeKey])
+		assert.Equal(t, standardClaims.Subject, claims[SubjectKey])
 	})
+}
+
+func TestGenerateJWTAndVerifyJWT(t *testing.T) {
+	t.Run("encode standard jwt inside map claims", func(t *testing.T) {
+		claims, err := InitializeMapClaims(
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*1).Unix(), // expiresAt
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*-1).Unix(), // issuedAt
+			GenerateRandomString(10),
+			time.Now().Unix()-50000, // notBefore
+			GenerateRandomString(10),
+		)
+		assert.Nil(t, err)
+		jwt, httpStatus, err := GenerateJWT(claims)
+		assert.Equal(t, http.StatusOK, httpStatus)
+		assert.Nil(t, err)
+		assert.True(t, strings.HasPrefix(jwt, "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9"))
+	})
+
+	t.Run("encode standard jwt with custom claims", func(t *testing.T) {
+		claims, err := InitializeMapClaims(
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*1).Unix(), // expiresAt
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*-1).Unix(), // issuedAt
+			GenerateRandomString(10),
+			time.Now().Unix()-50000, // notBefore
+			GenerateRandomString(10),
+		)
+		assert.Nil(t, err)
+
+		claims["custom"] = "value"
+		claims["author"] = "sean"
+
+		jwt, httpStatus, err := GenerateJWT(claims)
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, httpStatus)
+		assert.True(t, strings.HasPrefix(jwt, "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9"))
+	})
+
+	t.Run("verify audience", func(t *testing.T) {
+		audience := GenerateRandomString(10)
+		issuer := GenerateRandomString(10)
+		mapClaims, err := InitializeMapClaims(
+			audience,
+			time.Now().Add(time.Hour*1).Unix(),  // expiresAt
+			GenerateRandomString(10),            // id
+			time.Now().Add(time.Hour*-1).Unix(), // issuedAt
+			issuer,
+			time.Now().Unix()-50000,  // notBefore
+			GenerateRandomString(10), // subject
+		)
+		assert.Nil(t, err)
+
+		mapClaims["custom"] = "value"
+		mapClaims["author"] = "sean"
+
+		jwt, httpStatus, err := GenerateJWT(mapClaims)
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, httpStatus)
+		assert.True(t, strings.HasPrefix(jwt, "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9"))
+
+		retrievedClaims, httpStatus, err := VerifyJWT(jwt)
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, httpStatus)
+		assert.NotNil(t, retrievedClaims)
+
+		audienceVerified := retrievedClaims.VerifyAudience(audience, true)
+		assert.True(t, audienceVerified)
+
+		issuerVerified := retrievedClaims.VerifyIssuer(issuer, true)
+		assert.True(t, issuerVerified)
+	})
+
+	t.Run("jwt is expired", func(t *testing.T) {
+		claims, err := InitializeMapClaims(
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*-1).Unix(), // expiresAt
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*-1).Unix(), // issuedAt
+			GenerateRandomString(10),
+			time.Now().Unix()-50000, // notBefore
+			GenerateRandomString(10),
+		)
+		assert.Nil(t, err)
+
+		jwt, httpStatus, err := GenerateJWT(claims)
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, httpStatus)
+		assert.True(t, strings.HasPrefix(jwt, "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9"))
+
+		retrievedClaims, httpStatus, err := VerifyJWT(jwt)
+		assert.True(t, len(retrievedClaims) == 0)
+		assert.Equal(t, http.StatusUnauthorized, httpStatus)
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "Token is expired")
+	})
+
+	t.Run("issued in the future", func(t *testing.T) {
+		claims, err := InitializeMapClaims(
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*2).Unix(), // expiresAt
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*1).Unix(), // issuedAt
+			GenerateRandomString(10),
+			time.Now().Unix()-50000, // notBefore
+			GenerateRandomString(10),
+		)
+		assert.Nil(t, err)
+
+		jwt, httpStatus, err := GenerateJWT(claims)
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, httpStatus)
+		assert.True(t, strings.HasPrefix(jwt, "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9"))
+
+		retrievedClaims, httpStatus, err := VerifyJWT(jwt)
+		assert.True(t, len(retrievedClaims) == 0)
+		assert.Equal(t, http.StatusUnauthorized, httpStatus)
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "Token used before issued")
+	})
+
+	t.Run("used before allowed", func(t *testing.T) {
+		claims, err := InitializeMapClaims(
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*2).Unix(), // expiresAt
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*1).Unix(), // issuedAt
+			GenerateRandomString(10),
+			time.Now().Add(time.Hour*1).Unix(), // notBefore
+			GenerateRandomString(10),
+		)
+		assert.Nil(t, err)
+
+		jwt, httpStatus, err := GenerateJWT(claims)
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, httpStatus)
+		assert.True(t, strings.HasPrefix(jwt, "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9"))
+
+		retrievedClaims, httpStatus, err := VerifyJWT(jwt)
+		assert.True(t, len(retrievedClaims) == 0)
+		assert.Equal(t, http.StatusUnauthorized, httpStatus)
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "Token is not valid yet")
+	})
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func GenerateRandomString(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
 }
